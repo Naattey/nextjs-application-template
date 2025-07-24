57149b495c770d1ce51983623cead9771663b25a
Naattey <106297311+Naattey@users.noreply.github.com>
2025-07-24 22:18:26 +0500

    Add .next/static/chunks/src_app_reports_page_tsx_c582e7ae._.js 1753377505827

---
+++ .next/static/chunks/src_app_reports_page_tsx_c582e7ae._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_6dfc8ecc._.js",
+  "static/chunks/node_modules_4252adcb._.js"
+],
+    source: "dynamic"
+});
